6e1939e649442111af4d2652d3e729f4fb07b988
Dmitriy <4ebotarov_dim@inbox.ru>
2026-01-11 20:08:30 +0300

    include localhost

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-import exspress from "express"
+import express from "express";
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('hello dima!');
+});
+app.listen(4444,(error) => {
+    if(error){
+      return console.log(err)
+    }
+
+    console.log('server OK');
+});
+
+
