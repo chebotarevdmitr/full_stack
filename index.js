2d8d3ab3a23954a07e1942d3f211a2e6b112ab63
Dmitriy <4ebotarov_dim@inbox.ru>
2026-01-11 20:27:49 +0300

    Start:dev

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send('hello dima!');
+    res.send('Hello Dmitriy!');
 });
 app.listen(4444,(error) => {
     if(error){
